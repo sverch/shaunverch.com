f66a49629593aab9a0825986f05c3e8d83864186
Shaun Verch <shaun@shaunverch.com>
2019-10-27 01:54:28 -0400

    Deploy script updates
    This runs everything all at once, including updating the DNS, saving
    some time.

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+set -euo pipefail
+
 SERVICE_NAME="web-$(git rev-parse --short HEAD)"
-python example-static-site/helpers/deploy.py shaunverch-site consul-1 "$SERVICE_NAME" \
+pipenv run python example-static-site/helpers/deploy.py shaunverch-site consul-1 "$SERVICE_NAME" \
     shaunverch.com https://github.com/sverch/shaunverch.com/ "Shaun Verch"
+pipenv run cldls service get shaunverch-site "${SERVICE_NAME}"
+PUBLIC_IP="$(pipenv run cldls service get shaunverch-site "${SERVICE_NAME}" | grep public_ip | awk -F: '{print $2}' | tr -d '[:space:]')"
+OTHER_SERVICE="$(pipenv run cldls service ls | grep "Network: shaunverch-site, Service: web-" | grep -v "${SERVICE_NAME}" | awk '{print $4}' | tr -d '[:space:]')"
+OLD_PUBLIC_IP="$(pipenv run cldls service get shaunverch-site "${OTHER_SERVICE}" | grep public_ip | awk -F: '{print $2}' | tr -d '[:space:]')"
+echo "Old Service: ${OTHER_SERVICE}"
+pipenv run python example-static-site/helpers/update_dns.py shaunverch.com "${PUBLIC_IP}" --subdomain gce
+pipenv run python example-static-site/helpers/update_dns.py shaunverch.com "${OLD_PUBLIC_IP}" --subdomain gce --remove
+echo "When dns settles:"
+echo "cldls service destroy shaunverch-site ${OTHER_SERVICE}"
